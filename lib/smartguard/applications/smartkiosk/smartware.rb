f45137c690c6296ec1cb5b4eee5765198d4a10e4
Boris Staal <boris@roundlake.ru>
2012-12-17 13:39:10 +0700

    New dante interface for smartware

--- lib/smartguard/applications/smartkiosk/smartware.rb
+++ lib/smartguard/applications/smartkiosk/smartware.rb
@@ -3,18 +3,18 @@ module Smartguard
     class Smartkiosk
       class Smartware < Smartguard::Process
         def initialize(path)
-          @path      = path
-          @pids_path = path.join('tmp/pids')
-          @logs_path = path.join('log')
+          @path     = path
+          @pid_file = path.join('tmp/pids/smartware.pid')
+          @log_file = path.join('log/smartware.log')
         end
 
         def pid
-          File.read("#{@pids_path}/smartware.pid").to_i rescue nil
+          File.read(@pid_file).to_i rescue nil
         end
 
         def start
           Logging.logger.info "Starting smartware"
-          run @path, "bundle exec smartware piddir=#{@pids_path} logdir=#{@logs_path} &"
+          run @path, "bundle exec smartware -d --pid=#{@pid_file} --log=#{@log_file}"
         end
 
         def stop
