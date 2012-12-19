153092b5a544800f3fd3976011332aeb62d0e312
Boris Staal <boris@roundlake.ru>
2012-12-20 00:45:50 +0700

    Smartware requires config file

--- lib/smartguard/applications/smartkiosk/smartware.rb
+++ lib/smartguard/applications/smartkiosk/smartware.rb
@@ -3,9 +3,10 @@ module Smartguard
     class Smartkiosk
       class Smartware < Smartguard::Process
         def initialize(path)
-          @path     = path
-          @pid_file = path.join('tmp/pids/smartware.pid')
-          @log_file = path.join('log/smartware.log')
+          @path        = path
+          @pid_file    = path.join('tmp/pids/smartware.pid')
+          @log_file    = path.join('log/smartware.log')
+          @config_file = path.join('config/smartware.yml')
         end
 
         def pid
@@ -14,7 +15,7 @@ module Smartguard
 
         def start
           Logging.logger.info "Starting smartware"
-          run @path, "bundle exec smartware -d --pid=#{@pid_file} --log=#{@log_file}"
+          run @path, "bundle exec smartware -d --pid=#{@pid_file} --log=#{@log_file} --config-file=#{@config_file}"
         end
 
         def stop
