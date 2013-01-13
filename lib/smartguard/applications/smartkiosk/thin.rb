e3dcd10939610486b4449a4a7701cb6a57c565e0
Sergey Gridassov <grindars@gmail.com>
2013-01-13 19:26:46 +0400

    Start thin on port 3001 in development.

--- lib/smartguard/applications/smartkiosk/thin.rb
+++ lib/smartguard/applications/smartkiosk/thin.rb
@@ -7,16 +7,23 @@ module Smartguard
 
           Logging.logger.info "Starting thin"
 
+          if Smartguard.environment == :development
+            port = 3001
+          else
+            port = 3000
+          end
+
           if !run(@path,
                   {},
                   "bundle", "exec",
-                  "thin", "-e", Smartguard.environment.to_s, "start"
+                  "thin", "-e", Smartguard.environment.to_s, "-p", port.to_s,
+                  "start"
                  )
             return false
           end
 
           without_respawn do
-            wait_for_port 3000
+            wait_for_port port
           end
         end
 
