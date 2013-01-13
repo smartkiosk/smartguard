c8a5d7e8dd50e90e8721b663517394c598bbb245
Boris Staal <boris@roundlake.ru>
2013-01-13 23:24:12 +0700

    Better releases management for the development mode

--- lib/smartguard/application.rb
+++ lib/smartguard/application.rb
@@ -1,7 +1,10 @@
 module Smartguard
   class Application
+    attr_reader :releases_path, :current_path, :shared_path, :active_path
+
     def initialize(path)
-      @base_path     = Pathname.new(File.absolute_path path)
+      @base_path = Pathname.new(File.absolute_path path)
+
       if Smartguard.environment == :production
         @current_path  = @base_path.join('current')
         @releases_path = @base_path.join('releases')
@@ -9,7 +12,7 @@ module Smartguard
         @active_path   = File.readlink @current_path
       else
         @current_path  = @base_path
-        @releases_path = @base_path
+        @releases_path = @base_path.join('tmp/releases')
         @shared_path   = @base_path
         @active_path   = @base_path
       end
