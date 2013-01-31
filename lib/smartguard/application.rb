4d82a48afa4979ee2d3465ae1a07ba5e3cc1ee74
Sergey Gridasov <grindars@gmail.com>
2013-01-31 23:29:24 +0400

    Revert "Changed routing scheme."
    This reverts commit b8e09619573b4652d15ba22f47fbd77021e03b64.

--- lib/smartguard/application.rb
+++ lib/smartguard/application.rb
@@ -40,7 +40,7 @@ module Smartguard
       data = JSON.load data
 
       operation = ->() do
-        post_event "command.#{data["id"]}.started"
+        post_event 'command.started', data["id"]
 
         begin
           dispatch_command *data["command"]
@@ -51,9 +51,9 @@ module Smartguard
 
       callback = ->(result) do
         if result.respond_to? :exception
-          post_event "command.#{data["id"]}.finished", nil, result.to_s
+          post_event 'command.finished', data["id"], nil, result.to_s
         else
-          post_event "command.#{data["id"]}.finished", result
+          post_event 'command.finished', data["id"], result
         end
       end
 
